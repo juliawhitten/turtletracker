0c6542b220f750b81c175c9b0fec1011a2774483
juliawhitten <54548412+juliawhitten@users.noreply.github.com>
2019-09-30 12:58:20 -0400

    Sara No Header Loop v. 3

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -6,18 +6,29 @@
 # Created by: John Fay (john.fay@duke.edu)
 # Created on: Fall 2019
 #--------------------------------------------------------------
+<<<<<<< HEAD
 
 # Create a variable pointing to the file with no header
 fileName = "data/raw/SaraNoHeader.txt"
 # Open the file as a read-only file object
 fileObj = open(fileName, 'r')
+=======
+# Copy and paste a line of data as the lineString variable value
+lineString = "20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-126	529	3	401 651134.7	0"
+# Use the split command to parse the items in lineString into a list object
+lineData = lineString.split('\t')
+>>>>>>> parent of 3ffb507... Sara no header loop
 
-# Read in all lines in the text file into a list variable
-lineList = fileObj.readlines()
+recordID = lineData[0]              # ARGOS tracking record ID
+obsDateTime = lineData[2]           # Observation date and time (combined)
+obsDate = obsDateTime.split()[0]    # Observation date - first item in obsDateTime list object
+obsTime = obsDateTime.split()[1]    # Observation time - second item in obsDateTime list object
+obsLC = lineData[3]                 # Observation Location Class
+obsLat = lineData[5]                # Observation Latitude
+obsLon = lineData[6]                # Observation Longitude
 
-# Closes the file object (now that we have all we need)
-fileObj.close()
 
+<<<<<<< HEAD
 # Extract the first line from the lineList
 for lineString in lineList:
 
@@ -37,3 +48,6 @@ for lineString in lineList:
     print ("Record {0} indicates Sara was seen at {1}N and {2}W on {3}".
            format(recordID, obsLat,obsLat,obsDate))
     
+=======
+print ("Record {0} indicates Sara was seen at {1}N and {2}W on {3}".format(recordID, obsLat,obsLon,obsDate))
+>>>>>>> parent of 3ffb507... Sara no header loop
