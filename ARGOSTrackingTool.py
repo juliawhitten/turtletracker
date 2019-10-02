012e60586f8f17775d87499a774036f42afbe9b2
juliawhitten <54548412+juliawhitten@users.noreply.github.com>
2019-10-02 11:52:34 -0400

    Task5

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -6,31 +6,22 @@
 # Created by: John Fay (john.fay@duke.edu)
 # Created on: Fall 2019
 #--------------------------------------------------------------
-<<<<<<< HEAD
-
 # Create a variable pointing to the file with no header
 fileName = "data/raw/SaraNoHeader.txt"
+
 # Open the file as a read-only file object
 fileObj = open(fileName, 'r')
-=======
-# Copy and paste a line of data as the lineString variable value
-lineString = "20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-126	529	3	401 651134.7	0"
-# Use the split command to parse the items in lineString into a list object
-lineData = lineString.split('\t')
->>>>>>> parent of 3ffb507... Sara no header loop
 
-recordID = lineData[0]              # ARGOS tracking record ID
-obsDateTime = lineData[2]           # Observation date and time (combined)
-obsDate = obsDateTime.split()[0]    # Observation date - first item in obsDateTime list object
-obsTime = obsDateTime.split()[1]    # Observation time - second item in obsDateTime list object
-obsLC = lineData[3]                 # Observation Location Class
-obsLat = lineData[5]                # Observation Latitude
-obsLon = lineData[6]                # Observation Longitude
+# Read the first line from the open file object
+lineStrings = fileObj.readlines()
+print ("There are {} records in the file".format(len(lineStrings)))
 
+# Close the file object
+fileObj.close()
+# Create empty dictionaries
 
-<<<<<<< HEAD
-# Extract the first line from the lineList
-for lineString in lineList:
+# Use a for loop to read each line, one at a time, until the list is exhausted
+for lineString in lineStrings:
 
     # Use the split command to parse the items in lineString into a list object
     lineData = lineString.split("\t")
@@ -43,11 +34,8 @@ for lineString in lineList:
     obsLC = lineData[3]                 # Observation Location Class
     obsLat = lineData[5]                # Observation Latitude
     obsLon = lineData[6]                # Observation Longitude
+
+    # Add values to dictionary
     
-    # Print information to the user
-    print ("Record {0} indicates Sara was seen at {1}N and {2}W on {3}".
-           format(recordID, obsLat,obsLat,obsDate))
-    
-=======
-print ("Record {0} indicates Sara was seen at {1}N and {2}W on {3}".format(recordID, obsLat,obsLon,obsDate))
->>>>>>> parent of 3ffb507... Sara no header loop
+# Indicate script is complete
+print ("Finished")
